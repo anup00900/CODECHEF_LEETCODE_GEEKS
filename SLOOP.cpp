19838234bdb4af409154b96f61ad1823ac91b0c0
ANUP ROY <75533606+anup00900@users.noreply.github.com>
2021-05-11 00:52:59 +0530

    coldplay_codechef
    Chef is a big fan of Coldplay. Every Sunday, he will drive to a park taking M minutes to reach there, and during the ride he will play a single song on a loop. Today, he has got the latest song which is in total S minutes long. He is interested to know how many times will he be able to play the song completely.

---
+++ SLOOP.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	int s,m;
+	while(T--)
+	{
+	      cin>>m;
+	      cin>>s;
+	      
+	      int r = m/s;
+	      cout<<r<<endl;
+	      
+	      
+	      
+	      
+	      
+	}
+	
+	
+	
+	
+	
+	return 0;
+}
